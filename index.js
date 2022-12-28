bab2119cc4cc04f1d99ad571b6782d15b0901406
Yesho Simon <sepkap123@gmail.com>
2022-12-28 12:32:51 +0300

    created simple gantt display

--- index.js
+++ index.js
@@ -1,79 +1,90 @@
-var data = [
-  {date: "01/01/2016", pizzas: 10000},
-  {date: "01/02/2016", pizzas: 20000},
-  {date: "01/03/2016", pizzas: 40000},
-  {date: "01/04/2016", pizzas: 30000},
-  {date: "01/05/2016", pizzas: 30000},
-  {date: "01/06/2016", pizzas: 50000},
-  {date: "01/07/2016", pizzas: 30000},
-  {date: "01/08/2016", pizzas: 50000},
-  {date: "01/09/2016", pizzas: 60000},
-  {date: "01/10/2016", pizzas: 20000},
-  {date: "01/11/2016", pizzas: 10000},
-  {date: "01/12/2016", pizzas: 90000},
-];
-// Calculate Margins and canvas dimensions
-var margin = {top: 40, right: 40, bottom: 40, left: 60},
-  width = 700 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+var parseDate = d3.timeParse("%d-%b-%y");
 
-//Parsers and Formaters
-var parseTime = d3.timeParse("%d/%m/%Y");
-var formatTime = d3.timeFormat("%a/%b/%Y");
-// Scales
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+var data = [
+  {
+    category: "Weed Eucalyptus",
+    from: "1-Jan-17",
+    to: "15-Jan-17",
+    progress: 100,
+  },
+  {category: "Task 2", from: "13-Jan-17", to: "1-Feb-17", progress: 60},
+  {category: "Task 3", from: "1-Feb-17", to: "15-Feb-17", progress: 70},
+  {category: "Task 4", from: "10-Feb-17", to: "1-Mar-17", progress: 10},
+  {category: "Task 5", from: "1-Mar-17", to: "30-Mar-17", progress: 90},
+];
+
+var margin = {top: 50, right: 50, bottom: 50, left: 100},
+  width = 800 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+h = height - 50;
 var svg = d3
-  .select("body")
+  .select("#gantt")
   .append("svg")
-  .style("background-color", "#888")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//Arguments for axes : Ranges for X, y
-x.domain(
-  d3.extent(data, function (d) {
-    return parseTime(d.date);
+
+data.forEach(function (d) {
+  d.from = parseDate(d.from);
+  d.to = parseDate(d.to);
+});
+var x = d3.scaleTime().range([0, width]);
+x.domain([
+  d3.min(data, function (d) {
+    return d.from;
   }),
-);
-y.domain(
-  d3.extent(data, function (d) {
-    return d.pizzas / 1000;
+  d3.max(data, function (d) {
+    return d.to;
   }),
-);
-// Axes
+]);
+
+let y = d3
+  .scaleBand()
+  .domain(data.map((task) => task.category))
+  .range([0, height - margin.bottom]);
+
+let xAxis = d3
+  .axisBottom(x)
+  .tickFormat(d3.timeFormat("%b-%d"))
+  .tickSize(0)
+  .tickPadding(15);
+var yAxis = d3.axisLeft(y).ticks(data.length).tickSize(0).tickPadding(20);
+let color = d3.scaleLinear().domain([0, data]).range(["red", "blue"]);
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF".split("");
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 svg
   .append("g")
-  .attr("class", "axis axis--x")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
-svg.append("g").attr("class", "axis axis--y").call(d3.axisLeft(y));
-// Labels
-svg
-  .append("text")
-  .attr("text-anchor", "middle")
-  .style("font-size", "14px")
-  .attr(
-    "transform",
-    "translate(" + (margin.left - 94) + "," + height / 2 + ")rotate(-90)",
-  )
-  .text("Pizzas ( Thousands ) ");
+  .attr("class", "axis1")
+  .attr("transform", "translate(0," + h + ")")
+  .call(xAxis);
+svg.append("g").attr("class", "axis2").call(yAxis);
 
-svg
-  .append("text")
-  .style("font-size", "14px")
-  .attr("text-anchor", "middle")
-  .attr(
-    "transform",
-    "translate(" + width / 2 + "," + (height - (margin.bottom - 74)) + ")",
-  )
-  .text("Date");
-//  Chart Title
-svg
-  .append("text")
-  .attr("x", width / 2)
-  .attr("y", 20 - margin.top / 2)
-  .attr("text-anchor", "middle")
-  .style("font-size", "16px")
-  .text("Pizza consumption");
+let bars = svg
+  .append("g")
+  .selectAll("rect")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("y", function (d) {
+    return y(d.category);
+  })
+  .attr("height", height / data.length - 40)
+  .attr("x", function (d) {
+    return x(d.from);
+  })
+  .attr("width", function (d) {
+    return x(d.to) - x(d.from);
+  })
+  .attr("fill", function (d) {
+    return getRandomColor();
+  });
+
+bars.attr("transform", "translate(0,20)");
